468ffa58143cdac57f6996c32b348261378de8e1
sm.hassan90 <sm.hassan90@gmail.com>
2021-09-13 12:34:27 +0500

    side bar structure

--- assets/js/admin_main_page.js
+++ assets/js/admin_main_page.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
 
-    $('#channel_master').click(function(){
-
+    $('.menu-item').click(function(){
+        $(".menu-item").parent().removeClass("active");
+        $(this).parent().addClass("active");
     });
+
+    $('#channel_master_menu').click(function(){
+        show_hide_section_tab("#channel_master");
+    });
+    $('#department_master_menu').click(function(){
+        show_hide_section_tab("#department_master");
+    });
+
+
 });
+
+function show_hide_section_tab($class_name) {
+    $(".main-content").removeClass("active_block");
+    $(".main-content").addClass("hide_block");
+
+    $($class_name).removeClass("hide_block");
+    $($class_name).addClass("active_block");
+}
